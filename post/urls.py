1657b54abfacce729c3caaa88018b6a77292dea7
lllapuk <lllapukk@yandex.ru>
2021-05-15 00:04:36 +0300

    IO posts with images

--- post/urls.py
+++ post/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
 
-from .views import post_list
+from .views import (
+    post_list,
+    post_list_by_author,
+    add_post
+)
 
 
 urlpatterns = [
-    path('', post_list, name='post_list'),
+    path('posts/', post_list, name='post_list'),
+    path('posts/author/<int:user_id>/', post_list_by_author, name='post_list_by_author'),
+    path('posts/add/', add_post, name='add_post')
 ]
--- post/views.py
+++ post/views.py
@@ -4,6 +4,27 @@ from .models import Post
 
 
 def post_list(request):
-    post_list = Post.objects.all()
+    post_list = Post.objects.all().order_by('-pk')
 
     return render(request, 'post/list.html', {'post_list': post_list})
+
+
+def post_list_by_author(request, user_id):
+    post_list = Post.objects.filter(author=user_id).order_by('-pk')
+
+    return render(request, 'post/list.html', {'post_list': post_list})
+
+
+def add_post(request):
+    if request.method != 'POST':
+        return render(request, 'post/add.html')
+
+    header = request.POST.get('header')
+    text = request.POST.get('text')
+    image = request.FILES.get('image')
+
+    Post.objects.create(header=header, text=text, image=image, author=request.user)
+
+    return render(request, 'post/add.html', {
+        'header': header, 'text': text, 'image': image
+    })
